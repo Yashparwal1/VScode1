9364d736b7412e05c22fa9146fcb8caf35335d4c
Yash Parwal <yashparwal1@gmail.com>
2022-12-10 23:49:46 +0530

    new

--- python/pyhon projects/cartoon/main.py
+++ python/pyhon projects/cartoon/main.py
@@ -5,8 +5,7 @@ img = cv2.imread("cartoon/insta.jpg")
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 gray = cv2.medianBlur(gray, 5)
-edges = cv2.adaptiveThreshold(gray, cv2.ADAPTIVE_THRESH_MEAN_C, 
-                                    cv2.THRESH_BINARY, 9, 9)
+edges = cv2.adaptiveThreshold(gray, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9)
 
 color = cv2.bilateralFilter(img, 9, 250, 250)
 cartoon = cv2.bitwise_and(color, color, mask=edges)
